e63b7843884f2ea4e4b6e22a9de3e4566abc6635
Gionata Massi <gionata.massi@savoiabenincasa.it>
2018-02-16 12:57:29 +0100

    Codice sorgente esternalizzato. Visualizzazione traccia codice Python

--- algoritmi/ricerca/ricerca_lineare_iterativa.js
+++ algoritmi/ricerca/ricerca_lineare_iterativa.js
@@ -1,23 +1,23 @@
-const NON_PRESENTE = Symbol('non presente');
+const NON_PRESENTE = Symbol("non presente");
 
-const ricerca_lineare = function (A, n, v) {
-    let posizione = 0;
-    let elemento_corrente;
-    while (posizione < n) {
-        elemento_corrente = A[posizione];
-        if (elemento_corrente === v) {
-            return posizione;
-        } else {
-            posizione = posizione + 1;
-        }
+const ricerca_lineare = function(A, n, v) {
+  let posizione = 0;
+  let elemento_corrente;
+  while (posizione < n) {
+    elemento_corrente = A[posizione];
+    if (elemento_corrente === v) {
+      return posizione;
+    } else {
+      posizione = posizione + 1;
     }
+  }
 
-    return NON_PRESENTE;
-}
+  return NON_PRESENTE;
+};
 
+// Esempi
 const sequenza = [5, 1, 4, 2, 3];
 const dimensione = sequenza.length;
 let posizione;
 posizione = ricerca_lineare(sequenza, dimensione, 2);
 posizione = ricerca_lineare(sequenza, dimensione, 9);
-
--- algoritmi/ricerca/ricerca_lineare_iterativa.py
+++ algoritmi/ricerca/ricerca_lineare_iterativa.py
@@ -1,17 +1,20 @@
 NON_PRESENTE = -1
 
+
 def ricerca_lineare(A, n, v):
     posizione = 0
     while (posizione < n):
         elemento_corrente = A[posizione]
         if (elemento_corrente == v):
             return posizione
-            
-        posizione = posizione + 1
-        
+        else:
+            posizione = posizione + 1
     return NON_PRESENTE
-    
-sequenza =  [5, 1, 4, 2, 3]
+
+
+# Esempi
+sequenza = [5, 1, 4, 2, 3]
 dimensione = len(sequenza)
-ricerca_lineare(sequenza, dimensione, 2)
-ricerca_lineare(sequenza, dimensione, 9)
+posizione = 0
+posizione = ricerca_lineare(sequenza, dimensione, 2)
+posizione = ricerca_lineare(sequenza, dimensione, 9)
