ad64afd821fcb7804d5c845a5c25b9169b5eb13b
Gionata Massi <gionata.massi@savoiabenincasa.it>
2018-03-12 15:55:11 +0100

    Modifiche domande

--- appunti/algoritmi/ricerca/quiz/domande_cloze_ricerca.ts
+++ appunti/algoritmi/ricerca/quiz/domande_cloze_ricerca.ts
@@ -82,7 +82,9 @@ const quizTemplate = `<?xml version="1.0" encoding="UTF-8"?>
 let deposito: DepositoQuesiti = {
   quesiti: [
     {
-      categoria: nome_categoria("Ricerca lineare/Cloze quaderno-rubrica"),
+      categoria: nome_categoria(
+        "Ricerca lineare/Ricerca lineare - Cloze quaderno-rubrica"
+      ),
       quesiti: [
         {
           titolo: "Algoritmo di ricerca lineare",
@@ -131,7 +133,7 @@ criterio di ordinamento. Non sono note funzioni che mappano un nome in un indice
 <ul>
     <li>{{personaggio}} per cercare il numero di un amico nel sua quaderno nel modo pi&ugrave; efficiente possibile pu&ograve;
         usare l'algoritmo di ricerca {1:MULTICHOICE:=lineare~binaria~indicizzata}.</li>
-    <li>Nel caso pi&ugrave; fortunato, {{personaggio}} riesce a trovare il numero di un amico presente nel quaderno in {1:NUMERICAL:=1:0}
+    <li>Nel caso pi&ugrave; fortunato, {{personaggio}} riesce a trovare il numero di un amico presente nel quaderno in {1:MULTICHOICE:=1~10~(n+1)/2~n}
         s.
     </li>
     <li>Il tempo medio per trovare il numero di un amico presente nel quaderno &egrave; di {1:MULTICHOICE:1~10~=(n+1)/2~n} s.
@@ -152,7 +154,9 @@ criterio di ordinamento. Non sono note funzioni che mappano un nome in un indice
       ]
     },
     {
-      categoria: nome_categoria("Ricerca binaria/Cloze quaderno-rubrica"),
+      categoria: nome_categoria(
+        "Ricerca binaria/Ricerca binaria - Cloze quaderno-rubrica"
+      ),
       quesiti: [
         {
           titolo: "Algoritmo di ricerca binaria - cloze quaderno-rubrica",
@@ -191,7 +195,6 @@ tabella:</p>
             <td>determinare che un gruppo di pagine &egrave; composto di una sola pagina</td>
             <td>\\( 0 \\) s</td>
         </tr>
-        <li>determinare che in un tempo trascurabile (0 s);</li>
     </tbody>
 </table>
       
@@ -219,7 +222,7 @@ tabella:</p>
     },
     {
       categoria: nome_categoria(
-        "Ricerca indicizzata/Hash perfetto/Cloze quaderno-rubrica"
+        "Ricerca indicizzata/Hash perfetto/Ricerca indicizzata, hashing perfetto - Cloze quaderno-rubrica"
       ),
       quesiti: [
         {
@@ -277,7 +280,7 @@ con le prime due lettere del nome del contatto da ricercare.
     },
     {
       categoria: nome_categoria(
-        "Ricerca indicizzata/Hash con collisioni/Cloze quaderno-rubrica"
+        "Ricerca indicizzata/Hash con collisioni/Ricerca indicizata, hashing con collisioni - Cloze quaderno-rubrica"
       ),
       quesiti: [
         {
@@ -302,9 +305,39 @@ con le prime due lettere del nome del contatto da ricercare.
       <li>aprire la rubrica alla pagina corrispondente alla lettera iniziale di un nome in un tempo trascurabile (0 s);</li>
       <li>confrontare il nome da ricercare con quello scritto nella pagina della rubrica in un secondo (1 s);</li>
       <li>voltare pagina dalla precedente alla successiva in un tempo trascurabile (0 s);</li>
-      <li>determinare che sta analizzando l'ultima pagina corrispondente ad una lettera in un tempo trascurabile (0 s);</li>
-      <li>determinare che la pagina corrente &egrave; bianca in un tempo trascurabile (0 s).</li>
   </ul>
+
+  <p>Le azioni che {{personaggio}} pu&ograve; realizzare e i tempi che impiega per portale a termine sono indicati nella seguente
+    tabella:
+</p>
+
+<table>
+    <caption>Operazioni e tempi</caption>
+    <thead>
+        <tr>
+            <th>Operazione</th>
+            <th>Tempo</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>aprire il quaderno alla pagina corrispondente alla lettera iniziale di un nome</td>
+            <td>\\( 0 \\) s</td>
+        </tr>
+        <tr>
+            <td>leggere e confrontare il nome scritto nella pagina con quello da ricercare ovvero determinare che la pagina corrente &egrave; bianca</td>
+            <td>\\( 1 \\) s</td>
+        </tr>
+        <tr>
+          <td>determinare che sta analizzando l'ultima pagina corrispondente ad una lettera</td>
+          <td>\\( 0 \\) s</td>
+        </tr>
+        <tr>
+          <td>determinare che la pagina corrente &egrave; bianca</td>
+          <td>\\( 0 \\) s</td>
+        </tr>
+    </tbody>
+</table>
   
   <p><strong>Completare le seguenti affermazioni con la miglior approssimazione</strong>.</p>
   
@@ -328,7 +361,7 @@ con le prime due lettere del nome del contatto da ricercare.
     },
     {
       categoria: nome_categoria(
-        "Ricerca lineare/Decodifica dello pseudo-codice"
+        "Ricerca lineare/Ricerca lineare - Decodifica dello pseudo-codice"
       ),
       quesiti: [
         {
@@ -350,6 +383,10 @@ algoritmo (A, n, v):
 
 <p><strong>Scegli un'alternativa</strong></p>
 {1:MULTICHOICE_V:=Ricerca lineare~Ricerca binaria~Ricerca indicizzata~Riconoscimento dell'ordinamento}
+
+<p>L'algoritmo sopra descritto &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em></p>
+<p><strong>Scegli un'alternativa</strong></p>
+{1:MULTICHOICE_V:=Iterativa~Ricorsiva}
 `
         },
         {
@@ -372,13 +409,17 @@ algoritmo_ausiliare (A, n, v, indice):
   
   <p><strong>Scegli un'alternativa</strong></p>
   {1:MULTICHOICE_V:=Ricerca lineare~Ricerca binaria~Ricerca indicizzata~Riconoscimento dell'ordinamento}
-  `
+  
+  <p>L'algoritmo sopra descritto &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em>?</p>
+  <p><strong>Scegli un'alternativa</strong></p>
+  {1:MULTICHOICE_V:Iterativa~=Ricorsiva}
+`
         }
       ]
     },
     {
       categoria: nome_categoria(
-        "Ricerca binaria/Decodifica dello pseudo-codice"
+        "Ricerca binaria/Ricerca binaria - Decodifica dello pseudo-codice"
       ),
       quesiti: [
         {
@@ -404,7 +445,11 @@ algoritmo (A, n, v):
   
   <p><strong>Scegli un'alternativa</strong></p>
   {1:MULTICHOICE_V:Ricerca lineare~=Ricerca binaria~Ricerca indicizzata~Riconoscimento dell'ordinamento}
-  `
+  
+<p>L'algoritmo sopra descritto &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em>?</p>
+<p><strong>Scegli un'alternativa</strong></p>
+{1:MULTICHOICE_V:=Iterativa~Ricorsiva}
+`
         },
         {
           titolo: "Algoritmo di ricerca binaria - ricorsivo",
@@ -431,13 +476,17 @@ algoritmo (A, n, v):
     
     <p><strong>Scegli un'alternativa</strong></p>
     {1:MULTICHOICE_V:Ricerca lineare~=Ricerca binaria~Ricerca indicizzata~Riconoscimento dell'ordinamento}
-    `
+  
+    <p>L'algoritmo sopra descritto &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em>?</p>
+    <p><strong>Scegli un'alternativa</strong></p>
+    {1:MULTICHOICE_V:Iterativa~=Ricorsiva}
+`
         }
       ]
     },
     {
       categoria: nome_categoria(
-        "Ricerca indicizzata/Decodifica dello pseudo-codice"
+        "Ricerca indicizzata/Ricerca indicizzata, hashing perfetto - Decodifica dello pseudo-codice"
       ),
       quesiti: [
         {
@@ -456,13 +505,17 @@ algoritmo (A, n, v, h):
   
   <p><strong>Scegli un'alternativa</strong></p>
   {1:MULTICHOICE_V:Ricerca lineare~Ricerca binaria~=Ricerca indicizzata~Riconoscimento dell'ordinamento}
-  `
+  
+<p>L'algoritmo sopra descritto &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em>?</p>
+<p><strong>Scegli un'alternativa</strong></p>
+{1:MULTICHOICE_V:=Iterativa~Ricorsiva}
+`
         }
       ]
     },
     {
       categoria: nome_categoria(
-        "Riconoscimento di una sequenza ordinata/Decodifica dello pseudo-codice"
+        "Riconoscimento di una sequenza ordinata/Riconoscimento ordinamento - Decodifica dello pseudo-codice"
       ),
       quesiti: [
         {
@@ -484,6 +537,10 @@ algoritmo (A, n):
   
   <p><strong>Scegli un'alternativa</strong></p>
   {1:MULTICHOICE_V:Ricerca lineare~Ricerca binaria~Ricerca indicizzata~=Riconoscimento dell'ordinamento}
+
+<p>L'algoritmo sopra descritto &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em>?</p>
+<p><strong>Scegli un'alternativa</strong></p>
+{1:MULTICHOICE_V:=Iterativa~Ricorsiva}
   `
         },
         {
@@ -509,15 +566,69 @@ algoritmo_ausiliario (A, n , coppia)
     
     <p><strong>Scegli un'alternativa</strong></p>
     {1:MULTICHOICE_V:Ricerca lineare~Ricerca binaria~Ricerca indicizzata~=Riconoscimento dell'ordinamento}
+  
+<p>L'algoritmo sopra descritto &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em>?</p>
+<p><strong>Scegli un'alternativa</strong></p>
+{1:MULTICHOICE_V:Iterativa~=Ricorsiva}
     `
         }
       ]
     },
     {
-      categoria: nome_categoria("Ricerca binaria/Altri esempi"),
+      categoria: nome_categoria(
+        "Ricerca binaria/Riceca binaria - Esercizi di comprensione"
+      ),
       quesiti: [
         {
-          titolo: "Comprendere ed applicare l'algoritmo di ricerca binaria",
+          titolo:
+            "Comprendere ed applicare l'algoritmo di ricerca binaria - dizionario",
+          tipo: TipoQuesito.CLOZE,
+          istanziaPersonaggi: false,
+          testo: `
+<p>
+          Uno studente deve tradurre un testo dal latino usando un dizionario
+          latino/italiano di 512 pagine. I lemmi sono scritti nell'ordine alfabetico.
+          Lo studente conosce l'ordinamento dei lemmi e, dato un gruppo di pagine,
+          riesce ad aprire il dizionario nella pagina esattamente a met&agrave;.
+          Quante pagine dovrà aprire, al pi&ugrave;, per trovare il lemma che
+          sta cercando?
+
+          <br><em>Si suppone che usa la strategia pi&ugrave; efficiente</em>          
+</p>
+          
+  <p><strong>Scegli un'alternativa</strong></p>
+  {1:MULTICHOICE_V:8~=9~10~512}
+
+  `
+        },
+        {
+          titolo:
+            "Comprendere ed applicare l'algoritmo di ricerca binaria - indovina numero",
+          tipo: TipoQuesito.CLOZE,
+          istanziaPersonaggi: false,
+          testo: `
+<p>
+          Un sedicente mago si dice in grado di indovinare il numero pensato da
+          una persona tra il suo pubblico in \\( n \\) domande.
+          Il gioco segue sempre lo stesso schema: il mago chiede all'altra persona di
+          pensare un numero intero tra 1 e 1024. Poi pone \\( n \\) domande del tipo: 
+          \"Il numero che hai pensato &egrave; inferiore a ...?\" e quindi termina
+          dicendo il numero che l'altra persona aveva pensato.
+
+          Se il mago non ha poteri magici ma conosce e sa applicare gli algoritmi,
+          quanto vale \\( n \\)?
+
+          <br><em>Si suppone che usa la strategia pi&ugrave; efficiente</em>          
+</p>
+          
+  <p><strong>Scegli un'alternativa</strong></p>
+  {1:MULTICHOICE_V:8~9~10~=11}
+
+  `
+        },
+        {
+          titolo:
+            "Comprendere ed applicare l'algoritmo di ricerca binaria - ladro",
           tipo: TipoQuesito.CLOZE,
           istanziaPersonaggi: false,
           testo: `
@@ -526,7 +637,9 @@ algoritmo_ausiliario (A, n , coppia)
           combinazioni numeriche. La sua particolare tecnica gli consente di stabilire
           se la combinazione che sta provando precede o segue, nell'ordine numerico,
           quella corretta. Per aprire la cassaforte avr&agrave; al pi&ugrave; bisogno di
-          provare al pi&ugrave; quante combinazioni? </span>
+          provare al pi&ugrave; quante combinazioni?
+          
+          <br><em>Si suppone che usa la strategia pi&ugrave; efficiente</em>
 </p>
           
   <p><strong>Scegli un'alternativa</strong></p>
@@ -539,7 +652,7 @@ algoritmo_ausiliario (A, n , coppia)
     },
     {
       categoria: nome_categoria(
-        "Riconoscimento di una sequenza ordinata/Definizione"
+        "Riconoscimento di una sequenza ordinata/Riconoscimento ordinamento - Definizione"
       ),
       quesiti: [
         {
@@ -552,21 +665,20 @@ algoritmo_ausiliario (A, n , coppia)
 
   <p><strong>Scegli un'alternativa</strong></p>
 
-  {1:MULTICHOICE_V:=\\(\\mathrm{ordinato?\\}(S) = \\begin{cases\\}    \\mathrm{vero\\} & \\text{se \\}\\mathrm{lunghezza\\}(S)\\leq 1\\\\    \\mathrm{falso\\} & \\text{se \\}\\mathrm{primo\\}(S) \\succ \\mathrm{secondo\\}(S)\\\\    \\mathrm{ordinato?\\}(\\mathrm{resto\\}(S)) & \\text{altrimenti\\}\\end{cases\\}\\)~\\(\\mathrm{ordinato?\\}(S) = \\begin{cases\\}    \\mathrm{vero\\} & \\text{se \\}\\mathrm{lunghezza\\}(S)\\leq 1\\\\    \\mathrm{falso\\} & \\text{se \\}\\mathrm{primo\\}(S) > \\mathrm{secondo\\}(S)\\\\    \\mathrm{ordinato?\\}(S) &\\text{altrimenti\\}\\end{cases\\}\\)~\\(\\mathrm{ordinato?\\}(S) = \\begin{cases\\}    \\mathrm{vero\\} & \\text{se \\}\\mathrm{lunghezza\\}(S)\\leq 1\\\\    \\mathrm{falso\\}& \\text{se \\}\\mathrm{primo\\}(S)\\preccurlyeq\\mathrm{secondo\\}(S)\\\\    \\mathrm{ordinato?\\}(\\mathrm{resto\\}(S)) & \\text{altrimenti\\}\\end{cases\\}\\)~\\(\\mathrm{ordinato?\\}(S) = \\begin{cases\\}    \\mathrm{falso\\} & \\text{se \\}\\mathrm{lunghezza\\}(S)\\leq 1\\\\    \\mathrm{falso\\} & \\text{se \\}\\mathrm{primo\\}(S)\\succ\\mathrm{secondo\\}(S)\\\\    \\mathrm{ordinato?\\}(\\mathrm{resto\\}(S)) & \\text{altrimenti\\}\\end{cases\\}\\)}
+  {1:MULTICHOICE_V:=\\(\\mathrm{ordinato?\\}(S) = \\begin{cases\\}    \\mathrm{vero\\} & \\text{se \\}\\mathrm{lunghezza\\}(S)\\leq 1\\\\    \\mathrm{falso\\} & \\text{se \\}\\mathrm{primo\\}(S) \\succ \\mathrm{secondo\\}(S)\\\\    \\mathrm{ordinato?\\}(\\mathrm{resto\\}(S)) & \\text{altrimenti\\}\\end{cases\\}\\)~\\(\\mathrm{ordinato?\\}(S) = \\begin{cases\\}    \\mathrm{vero\\} & \\text{se \\}\\mathrm{lunghezza\\}(S)\\leq 1\\\\    \\mathrm{falso\\} & \\text{se \\}\\mathrm{primo\\}(S) \\succ \\mathrm{secondo\\}(S)\\\\    \\mathrm{ordinato?\\}(S) &\\text{altrimenti\\}\\end{cases\\}\\)~\\(\\mathrm{ordinato?\\}(S) = \\begin{cases\\}    \\mathrm{vero\\} & \\text{se \\}\\mathrm{lunghezza\\}(S)\\leq 1\\\\    \\mathrm{falso\\}& \\text{se \\}\\mathrm{primo\\}(S)\\preccurlyeq\\mathrm{secondo\\}(S)\\\\    \\mathrm{ordinato?\\}(\\mathrm{resto\\}(S)) & \\text{altrimenti\\}\\end{cases\\}\\)~\\(\\mathrm{ordinato?\\}(S) = \\begin{cases\\}    \\mathrm{falso\\} & \\text{se \\}\\mathrm{lunghezza\\}(S)\\leq 1\\\\    \\mathrm{falso\\} & \\text{se \\}\\mathrm{primo\\}(S)\\succ\\mathrm{secondo\\}(S)\\\\    \\mathrm{ordinato?\\}(\\mathrm{resto\\}(S)) & \\text{altrimenti\\}\\end{cases\\}\\)}
 
   <p>Nota: 
      \\( \\mathrm{lunghezza}(S) \\) restituisce il numero di elementi della sequenza \\( S \\) (e.g. \\( \\mathrm{lunghezza}([s_1, s_2, s_3]) = 3 \\)); <br>
+     \\( \\mathrm{primo}(S) \\) restituisce il primo elemento della sequenza \\( S \\) (e.g. \\( \\mathrm{primo}([s_1, s_2, s_3]) = s_1 \\)); <br>
+     \\( \\mathrm{secondo}(S) \\) restituisce il secondo elemento della sequenza \\( S \\) (e.g. \\( \\mathrm{secondo}([s_1, s_2, s_3]) = s_2 \\)); <br>
      \\( \\mathrm{resto}(S) \\) produce una sequenza \\(S^\\prime\\) che include gli elementi di \\(S\\) dal secondo all'ultimo, nello stesso ordine (e.g. \\( \\mathrm{resto}([s_1, s_2, s_3]) = [s_2, s_3] \\)). <br>
      \\(a \\preccurlyeq b\\) indica che \\(a\\) non segue \\(b\\) nell'ordinamento considerato mentre \\(a \\succ b\\) indica che \\(a\\)  segue \\(b\\).
-  `
-        }
-      ]
-    },
-    {
-      categoria: nome_categoria(
-        "Riconoscimento di una sequenza ordinata/Definizione"
-      ),
-      quesiti: [
+  
+<p>La definizione precedente &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em>?</p>
+<p><strong>Scegli un'alternativa</strong></p>
+     {1:MULTICHOICE_V:Iterativa~=Ricorsiva}
+`
+        },
         {
           titolo: "Definizione iterativa di ordinamento di una sequenza",
           tipo: TipoQuesito.CLOZE,
@@ -580,7 +692,11 @@ algoritmo_ausiliario (A, n , coppia)
   {1:MULTICHOICE_V:=\\(s_i \\preccurlyeq s_{i+1\\} \\text{ per \\} i = 1, 2, \\ldots, n - 1\\)~\\(s_i \\preccurlyeq s_{i+1\\} \\text{ per \\} i = 1, 2, \\ldots, n\\)~\\(s_i \\succ s_{i+1\\} \\text{ per \\} i = 1, 2, \\ldots, n - 1\\)~\\(s_i \\succ s_{i+1\\} \\text{ per \\} i = 1, 2, \\ldots, n\\)}
 
   <p>Nota: \\(a \\preccurlyeq b\\) indica che \\(a\\) non segue \\(b\\) nell'ordinamento considerato mentre \\(a \\succ b\\) indica che \\(a\\)  segue \\(b\\).
-  `
+  
+<p>L'algoritmo sopra descritto &egrave; in forma <em>iterativa</em> o <em>ricorsiva</em>?</p>
+<p><strong>Scegli un'alternativa</strong></p>
+{1:MULTICHOICE_V:=Iterativa~Ricorsiva}
+`
         }
       ]
     },
@@ -604,14 +720,28 @@ algoritmo_ausiliario (A, n , coppia)
       ]
     },
     {
-      categoria: nome_categoria("Ricerca indicizzata"),
+      categoria: nome_categoria(
+        "Ricerca indicizzata/Ricerca indicizzata - Esercizi di valutazione"
+      ),
       quesiti: [
         {
-          titolo: "Valutare le prestazioni",
+          titolo: "Valutare le prestazioni - hashing perfetto",
           tipo: TipoQuesito.CLOZE,
           istanziaPersonaggi: false,
           testo: `
-<p>Le tecniche di ricerca <em>indicizzate</em> che prevedono <em>collisioni</em> hanno prestazioni</p>
+<p>Le tecniche di ricerca <em>indicizzata</em> con <em>hashing perfetto</em> hanno prestazioni</p>
+
+  <p><strong>Scegli un'alternativa</strong></p>
+
+  {1:MULTICHOICE_V:che dipendono dal rapporto tra numero delle posizioni occupate e numero delle posizioni disponibili~che dipendono dal rapporto tra numero di scambi e numero di confronti~=costanti, ossia indipendenti dai dati~peggiori di quelle dell'algoritmo di ricerca lineare}
+  `
+        },
+        {
+          titolo: "Valutare le prestazioni - hashing con collisioni",
+          tipo: TipoQuesito.CLOZE,
+          istanziaPersonaggi: false,
+          testo: `
+<p>Le tecniche di ricerca <em>indicizzata</em> che prevedono <em>collisioni</em> hanno prestazioni</p>
 
   <p><strong>Scegli un'alternativa</strong></p>
 
