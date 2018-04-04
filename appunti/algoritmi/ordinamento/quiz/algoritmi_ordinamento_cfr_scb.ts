16aedff61d5ef0d0e01569b95f23393455d5f7fc
Gionata Massi <gionata.massi@savoiabenincasa.it>
2018-04-04 10:36:15 +0200

    Aggiunta della possibilita di operare su sequenze permutate

--- appunti/algoritmi/ordinamento/quiz/algoritmi_ordinamento_cfr_scb.ts
+++ appunti/algoritmi/ordinamento/quiz/algoritmi_ordinamento_cfr_scb.ts
@@ -1,109 +1,172 @@
+import { handlebars } from "handlebars";
+import { JSDOM } from "jsdom";
+
+export enum TipoOperazione {
+  inizio = "Inizio",
+  nuovaIterazione = "Nuova iterazione",
+  confronto = "confronto",
+  scambio = "scambio",
+  fine = "Fine"
+}
+
 export class Prestazioni {
-  private confronti: number;
-  private scambi: number;
+  private _confronti: number;
+  private _scambi: number;
+
   constructor(confronti = 0, scambi = 0) {
-    this.confronti = confronti;
-    this.scambi = scambi;
+    this._confronti = confronti;
+    this._scambi = scambi;
   }
-  incrementaConfronti() {
-    this.confronti++;
-  }
-  incrementaScambi() {
-    this.scambi++;
-  }
-}
 
-export class RigaRegistro {
-  private iterazione: number;
-  private sequenza: number[];
-  private prestazioniIterazione: Prestazioni;
-  private prestazioni: Prestazioni;
-  private ultimo_ordinato: number;
-  private confronti: Array<[number, number]>;
-  private scambi: Array<[number, number]>;
-  constructor(
-    sequenza: number[],
-    iterazione: number = 0,
-    ultimo_ordinato: number = 0,
-    confronti: Array<[number, number]>,
-    scambi: Array<[number, number]>,
-    prestazioni: Prestazioni
-  ) {
-    this.iterazione = iterazione;
-    this.sequenza = Object.assign([], sequenza);
-    this.ultimo_ordinato = ultimo_ordinato;
-    this.confronti = confronti;
-    this.scambi = scambi;
-    this.prestazioniIterazione = new Prestazioni(
-      confronti.length,
-      scambi.length
-    );
-    this.prestazioni = prestazioni
-      ? Object.assign({}, prestazioni)
-      : new Prestazioni();
+  incrementaConfronti(): void {
+    this._confronti++;
   }
-}
 
-export class Registro {
-  private riga: RigaRegistro[];
-  constructor() {
-    this.riga = [];
+  incrementaScambi(): void {
+    this._scambi++;
   }
-  public aggiungiRiga(riga: RigaRegistro) {
-    this.riga.push(riga);
+
+  get confronti(): number {
+    return this._confronti;
+  }
+
+  get scambi(): number {
+    return this._scambi;
   }
 }
 
 type CoppiaIndici = [number, number];
 
+export class RigaRegistro {
+  private _iterazione: number;
+  private _sequenza: number[];
+  private _prestazioniIterazione: Prestazioni;
+  private _prestazioni: Prestazioni;
+  private _ultimo_ordinato: number;
+  private _confronto: CoppiaIndici[];
+  private _scambio: CoppiaIndici[];
+  private _operazioni: TipoOperazione[];
+
+  constructor(
+    sequenza: number[],
+    iterazione: number = 0,
+    ultimo_ordinato: number = 0,
+    confronto: CoppiaIndici[],
+    scambio: CoppiaIndici[],
+    operazioni: TipoOperazione[],
+    prestazioni: Prestazioni
+  ) {
+    this._iterazione = iterazione;
+    this._sequenza = Object.assign([], sequenza);
+    this._ultimo_ordinato = ultimo_ordinato;
+    this._confronto = confronto;
+    this._scambio = scambio;
+    this._operazioni = operazioni;
+    this._prestazioniIterazione = new Prestazioni(
+      confronto.length,
+      scambio.length
+    );
+    this._prestazioni = prestazioni
+      ? new Prestazioni(prestazioni.confronti, prestazioni.scambi)
+      : new Prestazioni(0, 0);
+  }
+
+  public get iterazione(): number {
+    return this._iterazione;
+  }
+  public get sequenza(): number[] {
+    return this._sequenza;
+  }
+
+  public get confrontiIterazione(): number {
+    return this._prestazioniIterazione.confronti;
+  }
+
+  public get scambiIterazione(): number {
+    return this._prestazioniIterazione.scambi;
+  }
+
+  public get confrontiComplessivi(): number {
+    return this._prestazioni.confronti;
+  }
+
+  public get scambiComplessivi(): number {
+    return this._prestazioni.scambi;
+  }
+}
+
+export type Registro = RigaRegistro[];
+
+export class Traccia {
+  algoritmo: string;
+  sequenza_iniziale: number[];
+  traccia: Registro;
+}
+
 abstract class BaseSort {
   protected sequenza: number[];
+  protected sequenza_iniziale: number[];
   protected prestazioni: Prestazioni;
   protected registro: Registro;
   protected cfr: CoppiaIndici[];
   protected scb: CoppiaIndici[];
+  protected operazioni: TipoOperazione[];
   protected readonly nomeAlgoritmo: string;
 
   constructor(sequenza: number[], nome: string) {
     this.sequenza = sequenza;
+    this.sequenza_iniziale = Object.assign([], sequenza);
     this.prestazioni = new Prestazioni();
-    this.registro = new Registro();
+    this.registro = [];
     this.nomeAlgoritmo = nome;
+    this.operazioni = [];
   }
 
   protected precede(v: number[], i: number, j: number): boolean {
-    if (this.prestazioni) {
-      this.prestazioni.incrementaConfronti();
-      this.cfr.push([i, j]);
-    }
+    this.prestazioni.incrementaConfronti();
+    this.cfr.push([i, j]);
+    this.operazioni.push(TipoOperazione.confronto);
     return v[i] <= v[j];
   }
 
   protected scambia(v: number[], i: number, j: number): void {
     if (i == j) return;
-    if (this.prestazioni) {
-      this.prestazioni.incrementaScambi();
-      this.scb.push([i, j]);
-    }
+    this.prestazioni.incrementaScambi();
+    this.scb.push([i, j]);
+    this.operazioni.push(TipoOperazione.scambio);
     const tmp: number = v[i];
     v[i] = v[j];
     v[j] = tmp;
   }
 
-  protected inizioIterazione() {
-    this.cfr = [];
-    this.scb = [];
+  protected inizio(): void {
+    this.operazioni.push(TipoOperazione.inizio);
   }
 
-  public traccia() {
-    return this.registro;
+  protected fine(): void {
+    this.operazioni.push(TipoOperazione.fine);
+  }
+
+  protected inizioIterazione(): void {
+    this.cfr = [];
+    this.scb = [];
+    this.operazioni = [];
+    this.operazioni.push(TipoOperazione.nuovaIterazione);
+  }
+
+  public traccia(): Traccia {
+    return {
+      algoritmo: this.nome(),
+      sequenza_iniziale: this.sequenza_iniziale,
+      traccia: this.registro
+    };
   }
 
   public nome() {
     return this.nomeAlgoritmo;
   }
 
-  abstract ordina(): void;
+  abstract ordina(): Traccia;
 }
 
 export class SelectionSort extends BaseSort {
@@ -121,10 +184,11 @@ export class SelectionSort extends BaseSort {
     return indice_minimo_corrente;
   }
 
-  public ordina(): void {
+  public ordina(): Traccia {
+    this.inizio();
     let v: number[] = this.sequenza;
-    this.registro.aggiungiRiga(
-      new RigaRegistro(v, 0, -1, [], [], this.prestazioni)
+    this.registro.push(
+      new RigaRegistro(v, 0, -1, [], [], this.operazioni, this.prestazioni)
     );
 
     for (
@@ -135,17 +199,20 @@ export class SelectionSort extends BaseSort {
       this.inizioIterazione();
       const indice_minimo = this.seleziona_indice_minimo(v, primo_disordinato);
       this.scambia(v, primo_disordinato, indice_minimo);
-      this.registro.aggiungiRiga(
+      this.registro.push(
         new RigaRegistro(
           v,
           primo_disordinato + 1,
           primo_disordinato,
           this.cfr,
           this.scb,
+          this.operazioni,
           this.prestazioni
         )
       );
     }
+    this.fine();
+    return this.traccia();
   }
 }
 
@@ -166,10 +233,11 @@ export class InsertionSort extends BaseSort {
     }
   }
 
-  public ordina(): void {
+  public ordina(): Traccia {
+    this.inizio();
     let v: number[] = this.sequenza;
-    this.registro.aggiungiRiga(
-      new RigaRegistro(v, 0, 0, [], [], this.prestazioni)
+    this.registro.push(
+      new RigaRegistro(v, 0, 0, [], [], this.operazioni, this.prestazioni)
     );
 
     for (
@@ -180,17 +248,20 @@ export class InsertionSort extends BaseSort {
       this.inizioIterazione();
 
       this.inserisci(v, primo_disordinato);
-      this.registro.aggiungiRiga(
+      this.registro.push(
         new RigaRegistro(
           v,
           primo_disordinato,
           primo_disordinato,
           this.cfr,
           this.scb,
+          this.operazioni,
           this.prestazioni
         )
       );
     }
+    this.fine();
+    return this.traccia();
   }
 }
 
@@ -207,10 +278,11 @@ export class BubbleSort extends BaseSort {
     }
   }
 
-  public ordina(): void {
+  public ordina(): Traccia {
+    this.inizio();
     let v: number[] = this.sequenza;
-    this.registro.aggiungiRiga(
-      new RigaRegistro(v, 0, 0, [], [], this.prestazioni)
+    this.registro.push(
+      new RigaRegistro(v, 0, 0, [], [], this.operazioni, this.prestazioni)
     );
 
     for (
@@ -221,17 +293,20 @@ export class BubbleSort extends BaseSort {
       this.inizioIterazione();
 
       this.sali(v, primo_disordinato);
-      this.registro.aggiungiRiga(
+      this.registro.push(
         new RigaRegistro(
           v,
           primo_disordinato + 1,
           primo_disordinato,
           this.cfr,
           this.scb,
+          this.operazioni,
           this.prestazioni
         )
       );
     }
+    this.fine();
+    return this.traccia();
   }
 }
 
@@ -248,13 +323,198 @@ function test() {
     algoritmi.forEach(algoritmo => {
       let istanzaAlgoritmo = new algoritmo(Object.assign([], v));
       istanzaAlgoritmo.ordina();
-      soluzioni.push({
-        algoritmo: istanzaAlgoritmo.nome(),
-        sequenza: v,
-        traccia: istanzaAlgoritmo.traccia()
-      });
+      soluzioni.push(istanzaAlgoritmo.traccia());
     });
   });
 
   console.log(JSON.stringify(soluzioni));
 }
+
+function analizza(stringa) {
+  let ret = [],
+    pos = 0,
+    ultimoNumero;
+  while (pos < stringa.length) {
+    if (stringa[pos] >= "0" && stringa[pos] <= "9") {
+      ultimoNumero = pos + 1;
+      while (
+        ultimoNumero <= stringa.length &&
+        stringa[ultimoNumero] >= "0" &&
+        stringa[ultimoNumero] <= "9"
+      ) {
+        ultimoNumero++;
+      }
+      ret.push(parseInt(stringa.slice(pos, ultimoNumero), 10));
+      pos = ultimoNumero;
+    } else {
+      pos++;
+    }
+  }
+  return ret;
+}
+
+function creaTabella(traccia: Traccia): string {
+  const dom = new JSDOM(`<!DOCTYPE html>
+<html lang="it">
+ <head>
+  <meta charset="utf-8">
+  <title>
+  Algoritmo ${traccia.algoritmo} applicato alla sequenza ${
+    traccia.sequenza_iniziale
+  }
+  </title>
+  <style>
+  table {
+    border-collapse: collapse;
+  }
+  thead {
+    border-bottom: 1px solid black;
+  }
+  tfoot {
+    border-top: 1px solid black;
+  }
+  td:nth-child(4n+1) {
+    text-align: right;
+    font-size: bold;
+  }
+  td:nth-child(4n+2) {
+    text-align: center;
+    font-size: bold;
+  }
+  td:nth-child(4n+3) {
+    text-align: right;
+  }
+  td:nth-child(4n+4) {
+    text-align: right;
+  }
+  tbody tr:nth-child(2n+2) {
+    background-color: rgba(0, 128, 255, 0.25)
+  }
+  tfoot th {
+    text-align: right;
+  }
+  </style>
+ </head>
+ <body>
+  <table id="sequenze" style="width: 100%">
+   <caption>Algoritmo &quot;${
+     traccia.algoritmo
+   }&quot; applicato alla sequenza [${traccia.sequenza_iniziale}]</caption>
+   <thead id="intestazione">
+    <tr>
+     <th>Iterazione</th>
+     <th>Sequenza</th>
+     <th>Confronti</th>
+     <th>Scambi</th>
+    </tr>
+   </thead>
+   <tbody id="corpo"></tbody>
+   <tfoot id="intestazione">
+    <tr>
+     <th></th>
+     <th></th>
+     <th id="cfrComplessivi">Confronti</th>
+     <th id="scbComplessivi">Scambi</th>
+    </tr>
+   </tfoot>
+  </table>
+ </body>
+</html>`);
+  let tabella: HTMLElement = dom.window.document.getElementById("corpo");
+  const aggiungiRiga = function(
+    iterazione: number,
+    sequenza: number[],
+    nConfronti: number,
+    nScambi: number,
+    totConfronti: number,
+    totScambi: number
+  ) {
+    let tr: HTMLTableRowElement = dom.window.document.createElement("TR");
+    let aggiungiNumero = function(valore: number, classe: string) {
+      let td: HTMLTableDataCellElement = dom.window.document.createElement(
+        "TD"
+      );
+      td.innerHTML = valore.toString();
+      td.className = classe;
+      tr.appendChild(td);
+    };
+    let aggiungiSequenza = function(sequenza: number[], classe: string) {
+      let td: HTMLTableDataCellElement = dom.window.document.createElement(
+          "TD"
+        ),
+        j,
+        str = "";
+
+      if (sequenza.length) {
+        str += sequenza[0];
+      }
+      for (j = 1; j < sequenza.length; j++) {
+        str += " " + sequenza[j];
+      }
+      td.className = classe;
+      td.innerHTML = str;
+      tr.appendChild(td);
+    };
+    aggiungiNumero(iterazione, "iter");
+    aggiungiSequenza(sequenza, "seq");
+    aggiungiNumero(nConfronti, "cfr");
+    aggiungiNumero(nScambi, "swp");
+    tabella.appendChild(tr);
+  };
+  traccia.traccia.forEach(riga => {
+    aggiungiRiga(
+      riga.iterazione,
+      riga.sequenza,
+      riga.confrontiIterazione,
+      riga.scambiIterazione,
+      riga.confrontiComplessivi,
+      riga.scambiComplessivi
+    );
+  });
+  let ultimaRiga: RigaRegistro = traccia.traccia[traccia.traccia.length - 1];
+  let c: HTMLTableCellElement = dom.window.document.getElementById(
+    "cfrComplessivi"
+  );
+  c.innerHTML = ultimaRiga.confrontiComplessivi.toString();
+  let s: HTMLTableCellElement = dom.window.document.getElementById(
+    "scbComplessivi"
+  );
+  s.innerHTML = ultimaRiga.scambiComplessivi.toString();
+
+  return dom.serialize();
+}
+
+function permuta(v: number[], fnc: Function): void {
+  permuta_aux(v, 0, fnc);
+}
+
+function permuta_aux(v: number[], index: number, fnc: Function) {
+  if (index >= v.length - 1) {
+    return fnc(v);
+  }
+
+  for (let i: number = index; i < v.length; i++) {
+    //For each index in the sub array arr[index...end]
+
+    //Swap the elements at indices index and i
+    let t: number = v[index];
+    v[index] = v[i];
+    v[i] = t;
+
+    //Recurse on the sub array arr[index+1...end]
+    permuta_aux(v, index + 1, fnc);
+
+    //Swap the elements back
+    t = v[index];
+    v[index] = v[i];
+    v[i] = t;
+  }
+}
+
+// permuta([1, 2, 3, 4, 5], console.log);
+
+// console.log(creaTabella(new BubbleSort([9, 1, 2, 3, 4, 5, 6, 7, 8]).ordina()));
+
+//new SelectionSort([2, 3, 1, 4]).ordina().traccia.forEach(r => {
+//  console.log(JSON.stringify(r.sequenza));
+//});
